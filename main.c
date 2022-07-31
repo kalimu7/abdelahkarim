19c404d0c20bcb338c498d2db49089a5fc78242d
CS320 <lufy2024@gmail.com>
2022-07-31 23:49:07 +0100

    i added the 2 , 3 , 4 chalenge

--- main.c
+++ main.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
 int main()
 {
-    printf("type your code here\n");
+    //Challenge_Paire_Impaire();
+    //Challenge3_PermutationSousConditions();
+    Challenge4_some_and_triple();
+
 }
+
+void Challenge1_Paire_Impaire(){
+	int number;
+	bool isPaire = false;
+	printf("enter your number : ");
+	scanf("%d" , &number);
+
+	for(int i =2 ; i <=sqrt(number) ; i++){
+		if(number%i == 0){
+			isPaire = true;
+			break;
+		}
+	}
+
+	if(isPaire)
+        printf("this number is paire");
+    else
+        printf("this number is not paire");
+}
+
+void Challenge3_PermutationSousConditions(){
+    int n1 , n2 ;
+    printf("inter your first number :");
+    scanf("%d" , &n1);
+    printf("inter your second number :");
+    scanf("%d" , &n2);
+
+    if(n1 > n2) printf("%d > %d " , n1 , n2);
+    else  if(n1 < n2) printf("%d < %d " , n1 , n2);
+    else printf("%d = %d " , n1 , n2);
+}
+
+void Challenge4_some_and_triple(){
+     int n1 , n2 ;
+    printf("inter your first number :");
+    scanf("%d" , &n1);
+    printf("inter your second number :");
+    scanf("%d" , &n2);
+
+    if(n1 != n2)
+        printf("%d + %d = %d" , n1 , n2 , n1+n2);
+    else
+        printf("the triple of  %d = %d" , n1, 3*n1);
+}
+
+
+
