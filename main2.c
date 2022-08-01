11e03c0a195c9983028513fa85375803f028c22a
kalimu7 <mahjourkarim@gmail.com>
2022-08-01 14:24:43 +0100

    working on pand

--- main2.c
+++ main2.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    printf("type your code here\n");
+     char name[50];
+    printf("please enter a name,recommended to be palindrome \n ");
+    scanf("%s",name);
+    printf("the name you give is : %s\n",name);
+    printf("%c\n",name[0]);
+    for(int i=0;i<50;i++)
+    {
+        for(int j=50;j>i;j--)
+        {
+            name[i]==name[j];
+        }
+    } 
     return 0;
 }
