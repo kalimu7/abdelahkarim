af5b8697e9f65ad591e5e8c6be2f4e7464da0786
kalimu7 <mahjourkarim@gmail.com>
2022-08-01 23:32:50 +0100

    palindrome challenge is done

--- main2.c
+++ main2.c
@@ -1,18 +1,50 @@
-#include<stdio.h>
+//challenge of palindrome;
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool ispalindrome(char string[])
+{
+    int length = strlen(string);
+    int len = strlen(string)/2;
+    for(int i=0;i<len;i++){
+        if(string[i] == string[length-i-1]){
+        return true;
+        }
+      
+    }
+    return false;
+}
+
+
 
 int main()
 {
-     char name[50];
-    printf("please enter a name,recommended to be palindrome \n ");
-    scanf("%s",name);
-    printf("the name you give is : %s\n",name);
-    printf("%c\n",name[0]);
-    for(int i=0;i<50;i++)
+   
+    char name[] = "kayak";
+    char name2[] = "loyol";
+    char name3[] = "happiness";
+    //char name[] = "kayak";
+    if(ispalindrome(name))
     {
-        for(int j=50;j>i;j--)
-        {
-            name[i]==name[j];
-        }
-    } 
+        printf("%s is a palindrome\n",name);
+    }
+    else {
+        printf("%s is a not palindrome\n",name);
+    }
+     if(ispalindrome(name2))
+    {
+        printf("%s is a palindrome\n",name2);
+    }
+    else {
+        printf("%s is a not palindrome\n",name2);
+    }
+    if(ispalindrome(name3))
+    {
+        printf("%s is a palindrome\n",name3);
+    }
+    else {
+        printf("%s is a not palindrome\n",name3);
+    }
     return 0;
 }
