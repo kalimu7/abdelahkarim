4159b5b30997e8320b88fb5ef721a55dc1cb9f11
CS320 <lufy2024@gmail.com>
2022-08-04 21:08:23 +0100

    i have added the 12th challenge

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 int main()
 {
-    //Challenge1_Paire_Impaire();
+     //Challenge1_Paire_Impaire();
     //Challenge3_PermutationSousConditions();
     //Challenge4_some_and_triple();
     //Challenge5_Equation_deuxieme_degre();
@@ -16,7 +16,8 @@ int main()
     //Challenge8_Alphabet();
     //Challenge9_DateFormate();
     //Challenge10_RandomeDay();
-    Challenge11_TimeCompare();
+    //Challenge11_TimeCompare();
+    Challenge12_Point_onThe_Line();
 
 
 }
@@ -353,6 +354,85 @@ void Challenge11_TimeCompare(){
         }
     }
 }
+
+
+void Challenge12_Point_onThe_Line(){
+    puts("enter the coordinates of the Piece (X1 , Y1) ; (X2 , Y2)");
+
+    double x1 , x2 , y1 , y2;
+    puts("enter x1 :");
+    scanf("%lf" , &x1);
+
+    puts("enter y1 :");
+    scanf("%lf" , &y1);
+
+
+    puts("enter x2 :");
+    scanf("%lf" , &x2);
+
+    puts("enter y2 :");
+    scanf("%lf" , &y2);
+
+    puts("enter the coordinates of the Point (X ,Y) ");
+    double x , y;
+
+    puts("enter x :");
+    scanf("%lf" , &x);
+
+    puts("enter y :");
+    scanf("%lf" , &y);
+
+    printf("Coordinates of the piece ______________\n\n");
+    printf("X1=%lf  Y1=%lf                      X2=%lf   Y2=%lf \n\n" , x1,y1,x2,y2);
+     printf("Coordinates of the Point ______________\n\n\n");
+    printf("X=%lf       Y=%lf\n\n\n" , x,y);
+
+
+    /*------------------------------------------------------------
+    Y = ax + b donc en va calculer a et b
+    on sait que
+                        y2 - y1       deltaY
+                   a = ---------  = --------
+                        x2 - x1       deltaX
+
+    est en sait que poure b
+                                   deltaY
+                    b = y1 - x1* ---------
+                                   deltaX
+
+                                */
+
+    double a = (y2 - y1)/(x2 - x1)   ,  b =  y1 - x1*a;
+
+    printf("%lf\t%lf\n\n" , a,b) ; // ila b4iti t chof mo3adat al mosta9im
+
+
+    // tha909 ida kanat no9ta tantami ila al 9it3ato ::::
+
+
+    if(x>=x1 && x<=x2 || x<=x1 && x>=x2 ){
+        if(y>=y1 && y<=y2 || y<=y1 && y>=y2 ){
+            if((a*x + b - y) == 0 )
+                printf("this point belongs to the piece :) :) :)");
+            else printf("this point does not belongs to the piece :( :( :(");
+        }else printf("this point does not belongs to the piece :( :( :(");
+    }else printf("this point does not belongs to the piece :( :( :(");
+
+    puts("\n\n\n\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
